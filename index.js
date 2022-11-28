c480c4b7912cb8eb1551aaf516b1512e8b0826c0
dancun616 <dancun616@gmail.com>
2022-11-28 12:23:55 +0300

    completed message

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+const divide = function (){
+  const num1=2000
+  const num2 = 100
+  return (num1/num2)
+}
+let square = (num)=>{
+ let x = 2;
+ return(x*x)
+}
+let add =(num)=>{
+let a = 3 ;
+let b = 4
+return a + b
+}
